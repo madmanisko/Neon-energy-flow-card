8f54d68a710e196221a0205ad16cb3ad4fbf188e
Madman <t.zwolan@gmail.com>
2026-01-06 22:41:16 +0100

    another test

--- src/scene/scene-registry.ts
+++ src/scene/scene-registry.ts
@@ -6,11 +6,12 @@ import { SCENE_V1 } from "./scene-v1";
  */
 export const SCENE_OPTIONS = [
   { value: "wide_v1", label: "Wide 16:9 ver V1" },
-  { value: "wide_v1", label: "Wide 16:9 ver V1test" }
+  { value: "wide_v1_test", label: "Wide 16:9 ver V1test" }
 ] as const;
 
 export type SceneId = (typeof SCENE_OPTIONS)[number]["value"];
 
 export const SCENES: Record<SceneId, any> = {
-  wide_v1: SCENE_V1
+  wide_v1: SCENE_V1,
+  wide_v1_test: SCENE_V1
 };
